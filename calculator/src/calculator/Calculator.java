af7299e45cc13f24ddabae84babeb854e3c28004
USER <USER@USER11-PC>
2017-01-17 10:05:50 +0900

    Add sub function

--- calculator/src/calculator/Calculator.java
+++ calculator/src/calculator/Calculator.java
@@ -3,6 +3,14 @@ package calculator;
 public class Calculator {
 	public int add(int num1 ,  int num2){
 		int result=0;
+		result = num1+num2;
 		return result;
+		
+	}
+	public int sub(int num1 ,  int num2){
+		int result=0;
+		result = num1-num2;
+		return result;
+		
 	}
 }
